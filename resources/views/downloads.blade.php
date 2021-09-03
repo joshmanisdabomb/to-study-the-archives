251484020318174a839af80b4ac55f6820cd777a
Josh <joshmanisdabomb@gmail.com>
2021-09-03 17:05:01 +0100

    Duration displayed on downloads instead of date.

--- resources/views/downloads.blade.php
+++ resources/views/downloads.blade.php
@@ -15,9 +15,9 @@
                         @foreach ($group->versions as $version)
                             <div class="w-full lg:flex items-center">
                                 <p>
-                                    {{ $version->mod_version }}{{ $version->title ? (' (' . $version->title . ')') : '' }} for Minecraft {{ $version->mc_version }}, released on {{ $version->released_at->format('d/m/Y H:i:s') }}
+                                    {{ $version->mod_version }}{{ $version->title ? (' (' . $version->title . ')') : '' }} for Minecraft {{ $version->mc_version }}, released <span class="border-dashed border-black" style="border-bottom-width: 1px" title="{{ $version->released_at->format('d/m/Y H:i:s') }}">{{ $version->released_at->diffForHumans() }}</span>
                                 </p>
-                                <div class="flex-grow text-right py-1">
+                                <div class="flex-grow lg:text-right py-1">
                                     <a href="https://bitbucket.org/joshmanisdabomb/loosely-connected-concepts/downloads/{{ $group->code }}-{{ $version->mc_version }}-{{ $version->code }}.jar" class="bg-blue-600 hover:bg-blue-800 text-white py-2 px-4 rounded inline-flex items-center">
                                         <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
                                         <span>Download from BitBucket</span>
